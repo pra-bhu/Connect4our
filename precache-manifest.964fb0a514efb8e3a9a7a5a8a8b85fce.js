d4f2602330d6b2e5708144f4a01d4ca524e0c3c4
pra-bhu <prashantbhushan@live.com>
2019-05-27 03:16:45 +0530

    Updates

---
+++ precache-manifest.964fb0a514efb8e3a9a7a5a8a8b85fce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9eeecf4d61326f66ae8ffedd0af0557",
+    "url": "/pra-bhu/connect4our.git/index.html"
+  },
+  {
+    "revision": "843b6f59e1a8ba461fde",
+    "url": "/pra-bhu/connect4our.git/static/css/main.05652be2.chunk.css"
+  },
+  {
+    "revision": "3e67784a621cbf4e8b20",
+    "url": "/pra-bhu/connect4our.git/static/js/2.1ffb9391.chunk.js"
+  },
+  {
+    "revision": "843b6f59e1a8ba461fde",
+    "url": "/pra-bhu/connect4our.git/static/js/main.defc8b01.chunk.js"
+  },
+  {
+    "revision": "59a114ea74f95e1ddcc1",
+    "url": "/pra-bhu/connect4our.git/static/js/runtime~main.6bb1aa93.js"
+  }
+]);
